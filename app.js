937fadb0051c5e5d3170720ffb6a3afdb01f168a
Tra <xzhu@college.harvard.edu>
2019-11-03 23:34:37 -0500

    value filter implemented

--- app.js
+++ app.js
@@ -4,56 +4,68 @@ var request = require("request");
 app.set("view engine", "ejs");
 app.use(express.static("assets"));
 
-
+// home/search page
 app.get("/", function(req, res) {
     res.render("search");
-});
+})
 
-
+// results page
 app.get("/results", function(req, res) {
     var keyword = req.query.keyword;
-    console.log(keyword);
+    var value = req.query.value;
     // use jService API: jservice.io
     var url = "http://jservice.io/api/clues/";
     // parse and display results
     request(url, function(error, response, body) {
         if (!error && res.statusCode == 200) {
             var parsedData = JSON.parse(body);
-            console.log(parsedData.length);
             var parsedResults = [];
             var categories = [];
-            var dates = [];
             var values = [];
+            var hasValFilter = false;
+            if (value !== "0") hasValFilter = true;
             for (var i in parsedData) {
+                // check for keyword match
                 if (parsedData[i].question.includes(keyword) || parsedData[i].category.title.includes(keyword)) {
-                    parsedResults.push(parsedData[i]);
-                    console.log(new Date(parsedData[i].airdate));
-                    dates.push(new Date(parsedData[i].airdate));
-                    if (!categories.includes(parsedData[i].category.title)) {
-                        categories.push(parsedData[i].category.title);
+                    // if value filter exists, check for filter match
+                    if (hasValFilter) {
+                        if (parsedData[i].value == value) {
+                            parsedResults.push(parsedData[i]);
+                            if (!categories.includes(parsedData[i].category.title)) {
+                                categories.push(parsedData[i].category.title);
+                            }
+                            if (!values.includes(parsedData[i].value)) {
+                                values.push(parsedData[i].value);
+                            }
+                        }
                     }
-                    if (!values.includes(parsedData[i].value)) {
-                        values.push(parsedData[i].value);
+                    else {
+                        parsedResults.push(parsedData[i]);
+                        if (!categories.includes(parsedData[i].category.title)) {
+                            categories.push(parsedData[i].category.title);
+                        }
+                        if (!values.includes(parsedData[i].value)) {
+                            values.push(parsedData[i].value);
+                        }
                     }
                 }
             }
-            console.log(parsedResults.length);
-            console.log(categories);
-            console.log(dates);
+            // if no results match, render notFound page
             if (parsedResults.length < 1) {
                 res.render("notFound");
             }
             res.render("results", {data: parsedResults, categories: categories, values: values});
-        }
+            }
         else {
             res.send("API connection failed");
         }
-    });
-});
+    })
+})
 
 
+// random clues page
 app.get("/random", function(req, res) {
-    var url = "http://jservice.io/api/random/?count=9";
+    var url = "http://jservice.io/api/random/?count=10";
     request(url, function(error, response, body) {
         if (!error && res.statusCode == 200) {
             var parsedData = JSON.parse(body);
@@ -75,10 +87,16 @@ app.get("/random", function(req, res) {
         else {
             res.send("API connection failed");
         }
-    });
-});
+    })
+})
+
+
+// contact page
+app.get("/contact", function(req, res) {
+    res.render("contact");
+})
 
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("The server has successfully set up.");
-});
+})
